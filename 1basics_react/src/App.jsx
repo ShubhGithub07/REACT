aa8d72d25e0fea2362445aa5344ccd3f8e00b255
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-02-01 19:24:10 +0530

    counter

--- 1basics_react/src/App.jsx
+++ 1basics_react/src/App.jsx
@@ -1,11 +1,40 @@
 import Try from "./Try"
+import { useState } from "react";
 
 function App() {
+  // const username = "Good Morning"
+  // let count = 15
+  let [count, setCount] = useState(10)
+
+  const addValue = () => {
+    console.log("value added", count);
+    // count = count + 1
+    if (count >= 20) {
+      console.log({count});
+    } else {
+      setCount(count + 1)
+    }
+  }
+
+  const removeValue = () => {
+    console.log("value removed", count);
+    
+    if (count <= 0) {
+      console.log({count});
+    }
+    else {
+      setCount(count - 1)  
+    }
+  }
+
 
   return (
     <>
-      <h1>Hello World!</h1>
-      <Try/>
+      {/* <h1>Hello World! {username}</h1> */}
+      {/* <Try/> */}
+      <h2>Counter value: {count}</h2>
+      <button onClick={addValue}>Add value {count}</button><br />
+      <button onClick={removeValue}>Remove value</button>
     </>
   )
 }
