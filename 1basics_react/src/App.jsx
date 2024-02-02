c5a8ed41ca634e64241c2bd41665a1a9ad58f5af
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-02-02 20:38:24 +0530

    customHook

--- 1basics_react/src/App.jsx
+++ 1basics_react/src/App.jsx
@@ -2,11 +2,12 @@ import Try from "./Try"
 import { useState } from "react";
 import Card from "./components/Card";
 import Password from "./components/Password";
+import InputBox from "./components/InputBox";
 
 function App() {
   // const username = "Good Morning"
   // let count = 15
-  let [color, setColor] = useState("olive")
+  let [color, setColor] = useState("white")
   let [count, setCount] = useState(10)
 
   const addValue = () => {
@@ -40,7 +41,7 @@ function App() {
       <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
         <h1 className="bg-green-400 text-white p-4 rounded-xl">Tailwind test</h1>
         <Card username="Shubham" />
-        <Card username="Max" />
+        {/* <Card username="Max" /> */}
 
         <h2>Counter value: {count}</h2>
         <button onClick={addValue}>Add value {count}</button><br />
@@ -48,8 +49,9 @@ function App() {
 
         <Password />
 
+        <InputBox />
         
-        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        {/* <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
             <button onClick={() => setColor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "red"}}>Red</button>
             <button onClick={() => setColor("orange")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "orange"}}>Orange</button>
@@ -57,7 +59,7 @@ function App() {
             <button onClick={() => setColor("green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}}>Green</button>
             <button onClick={() => setColor("blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}}>Blue</button>
           </div>
-        </div>
+        </div> */}
       </div>
 
       {/* <h1>Hello World! {username}</h1> */}
---
+++ 1basics_react/src/CustomHooks/Currencyinfo.js
@@ -0,0 +1,16 @@
+// JS is important for custom hooks, unspoken rule of hooks is to use 'use'
+
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+    }, [currency])
+
+    return data
+}
+
+export default useCurrencyInfo;
