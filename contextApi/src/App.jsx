e5479aff1ae7a9822c679135c0323cbcbabd3584
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-02-03 19:18:27 +0530

    themeSwitcher

--- contextApi/src/App.jsx
+++ contextApi/src/App.jsx
@@ -1,19 +1,49 @@
-
-import './App.css'
-import Login from './components/Login'
-import Profile from './components/Profile'
-import UserContextProvider from './context/UserContextProvider'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./components/Card";
+// import Login from "./components/Login";
+// import Profile from "./components/Profile";
+import ThemeBtn from "./components/ThemeBtn";
+import { ThemeProvider } from "./context/Theme";
+// import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
-  
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <UserContextProvider>
-      <h1>Context API</h1>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      {/* <h1>Context API</h1>
       <Login />
-      <Profile />
-    </UserContextProvider>
-  )
+      <Profile /> */}
+
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
