c1f507aa4de3a2e28461eac8667734f7715a82f3
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-02-05 10:20:14 +0530

    JSON

--- todoList/src/components/TodoForm.jsx
+++ todoList/src/components/TodoForm.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import { useTodo } from "../context";
+import { useTodo } from "../context/TodoContext";
 
 function TodoForm() {
-    const [todo, setTodo] = useState("")
-    const {addTodo} =useTodo()
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
 
-    const add = (e) => {
-        e.preventDefault()
+  const add = (e) => {
+    e.preventDefault();
 
-        if (!todo) return
+    if (!todo) return;
 
-        addTodo({todo, completed: false})
-        setTodo("")
-    }
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
 
   return (
     <form onSubmit={add} className="flex">
@@ -21,7 +21,7 @@ function TodoForm() {
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
         value={todo}
-        onChange={(e) => setTodo(e.target.vlaue)}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button
         type="submit"
