73c51000809b1ec48ab5d74a41ac0909e2fa48ea
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-02-27 20:20:03 +0530

    auth snippet for appwrite

--- appWriteBlog/src/appwrite/auth.js
+++ appWriteBlog/src/appwrite/auth.js
@@ -17,9 +17,36 @@ export class AuthService {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
             if (userAccount) {
                 // call another method
+                return this.login({email, password});
             } else {
                 return userAccount;
             }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
         } catch (error) {
             throw error;
         }
