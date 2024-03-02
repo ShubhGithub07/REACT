0f32387826d81d7d01c4d7d9a15cff4ed713e9f0
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-03-02 19:03:54 +0530

    authSlice

---
+++ appWriteBlog/src/store/store.js
@@ -0,0 +1,12 @@
+import {configureStore} from '@reduxjs/toolkit';
+export const store = configureStore({
+    reducer: {
+
+    }
+})
+
+// import {configureStore} from '@reduxjs/toolkit';
+// import todoReducer from '../features/todo/todoSlice';
+// export const store = configureStore({
+//     reducer: todoReducer
+// })
