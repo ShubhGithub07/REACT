9cd4f6b1227ba04befb339a1e8b253a672354e77
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-03-08 19:51:44 +0530

    debugging_cors error is remaining

--- appWriteBlog/src/store/store.js
+++ appWriteBlog/src/store/store.js
@@ -1,9 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit';
-const store = configureStore({
-    reducer: {
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
 
-    }
-})
+const store = configureStore({
+  reducer: {
+    auth: authSlice,
+    // post: postSlice,
+  },
+});
 
 export default store;
 
